59fb276f0c09a779ee20f37d9be7fc411a0beddb
KONRAD Frederic <fred.konrad@greensocs.com>
2015-09-15 16:10:41 +0200

    dualportmemory: fix the offset.
    The offset was wrong before because the b_transact function isn't aware of which
    socket has made the access. A tlm_b_if_wrapper is needed to allow to specify an
    other callback.
    
    Signed-off-by: KONRAD Frederic <fred.konrad@greensocs.com>

--- include/SimpleMemory/DualPortMemory.h
+++ include/SimpleMemory/DualPortMemory.h
@@ -19,8 +19,7 @@
 
 class DualPortMemory:
   public sc_core::sc_module,
-  public BaseMemory,
-  public gs::tlm_multi_b_if<gs::gp::GenericSlaveAccessHandle>
+  public BaseMemory
 {
 public:
     DualPortMemory(sc_core::sc_module_name name):
@@ -31,26 +30,35 @@ public:
     m_size("size", 1000),
     m_ro("read_only", false)    
     {
-        this->targetPort0.bind_b_if(*this);
+        wrap_1.register_b_transport(&DualPortMemory::b_transact_0, this);
+        this->targetPort0.bind_b_if(wrap_1);
         this->targetPort0.register_get_direct_mem_ptr(this,
             &DualPortMemory::get_direct_mem_ptr);
 
-        this->targetPort1.bind_b_if(*this);
+        wrap_2.register_b_transport(&DualPortMemory::b_transact_1, this);
+        this->targetPort1.bind_b_if(wrap_2);
         this->targetPort1.register_get_direct_mem_ptr(this,
             &DualPortMemory::get_direct_mem_ptr);
     }
 
-    void b_transact(gs::gp::GenericSlaveAccessHandle ah, unsigned int index)
+    void b_transact_0(gs::gp::GenericSlaveAccessHandle ah)
     {
         uint32_t offset;
         gs::gp::GenericSlavePort<32>::accessHandle t =
             _getSlaveAccessHandle(ah);
-        if(index == 0) {
-            offset = t->getMAddr() - targetPort0.base_addr;
-        } else {
-            offset = t->getMAddr() - targetPort1.base_addr;
-        }
-        b_transact(t, offset);
+
+        offset = t->getMAddr() - targetPort0.base_addr;
+        BaseMemory::b_transact(t, offset);
+    }
+
+    void b_transact_1(gs::gp::GenericSlaveAccessHandle ah)
+    {
+        uint32_t offset;
+        gs::gp::GenericSlavePort<32>::accessHandle t =
+            _getSlaveAccessHandle(ah);
+
+        offset = t->getMAddr() - targetPort1.base_addr;
+        BaseMemory::b_transact(t, offset);
     }
 
     void end_of_elaboration()
@@ -65,6 +73,10 @@ public:
   private:
     gs::gs_param<uint32_t> m_size;
     gs::gs_param<bool> m_ro; /*!< read only? */
+    gs::tlm_b_if_wrapper<DualPortMemory,
+                         gs::gp::GenericSlaveAccessHandle> wrap_1;
+    gs::tlm_b_if_wrapper<DualPortMemory,
+                         gs::gp::GenericSlaveAccessHandle> wrap_2;
 };
 
 #endif /* DUAL_PORT_MEMORY_H */
